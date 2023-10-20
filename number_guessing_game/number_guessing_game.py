50634c67ddf51ea4df04e72b630057904bc3cad4
Joshua Hecker <jhecker2001@gmail.com>
2023-10-20 08:52:40 -0500

    Included difficulty selection.

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -6,3 +6,10 @@ print(art.logo)
 player_guess = 0
 target_number = random.randint(1,100)
 
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+lives = 0
+if difficulty == "easy":
+    lives = 10
+elif difficulty == "hard":
+    lives - 5
+    
