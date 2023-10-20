4520e9a2e37c0b68716fa9b95049177e9dfa8a7c
Joshua Hecker <jhecker2001@gmail.com>
2023-10-20 09:08:40 -0500

    Added comments.

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -1,13 +1,16 @@
-import art
-import random
+import art      # Imports ASCII art for the logo
+import random   # Imports random module to generate random number.
 
+# Display ASCII logo and the intro.
 print(art.logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100!")
 
+# Set the initial guess to 0, and randomly generate the number to guess.
 player_guess = 0
 target_number = random.randint(1,100)
 
+# Get the user to choose a difficulty and give them the appropriate number of guesses.
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 lives = 0
 if difficulty == "easy":
@@ -15,9 +18,15 @@ if difficulty == "easy":
 elif difficulty == "hard":
     lives = 5
     
+# The game goes on until the player correctly guesses the number or runs out of lives.
 while player_guess != target_number and lives > 0:
+    # Display the number of guesses remaining.
     print(f"You have {lives} attempts remaining to guess the number.")
+    # Get the user to guess the number.
     player_guess = int(input("Make a guess: "))
+    
+    # If the player got the guess right, display a congratulatory message.
+    # Otherwise, display the appropriate feedback and subtract a life from the player.
     if player_guess == target_number:
         print(f"You got it! The answer was {target_number}.")
     else:
@@ -27,6 +36,7 @@ while player_guess != target_number and lives > 0:
             print("Too low.")
         print("Guess again.")
         lives -= 1
-        
+     
+    # If the player ran out of lives, give a game over message and display the number they were supposed to guess.
     if lives == 0:
-        print("You've run out of guesses, you lose.")
+        print(f"You've run out of guesses, you lose. The answer was {target_number}.")
