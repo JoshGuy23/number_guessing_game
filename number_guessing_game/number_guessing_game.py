2b7314aac98c8558b7f689530c5aa3b631dad4da
Joshua Hecker <jhecker2001@gmail.com>
2023-10-20 09:00:09 -0500

    Created ability for player to guess the number and provide feedback based on their answer, as well as feedback depending if they've found the answer or run out of guesses.

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -16,4 +16,17 @@ elif difficulty == "hard":
     lives - 5
     
 while player_guess != target_number and lives > 0:
-    
+    print(f"You have {lives} attempts remaining to guess the number.")
+    player_guess = int(input("Make a guess: "))
+    if player_guess == target_number:
+        print(f"You got it! The answer was {target_number}.")
+    else:
+        if player_guess > target_number:
+            print("Too high.")
+        else:
+            print("Too low.")
+        print("Guess again.")
+        lives -= 1
+        
+    if lives == 0:
+        print("You've run out of guesses, you lose.")
