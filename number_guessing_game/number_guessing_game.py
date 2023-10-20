7d73b68e660f9658b52b51521125b5498c7623fb
Joshua Hecker <jhecker2001@gmail.com>
2023-10-20 09:18:55 -0500

    Condensed some lines of code into functions.

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -10,23 +10,9 @@ print("I'm thinking of a number between 1 and 100!")
 player_guess = 0
 target_number = random.randint(1,100)
 
-# Get the user to choose a difficulty and give them the appropriate number of guesses.
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-lives = 0
-if difficulty == "easy":
-    lives = 10
-elif difficulty == "hard":
-    lives = 5
-    
-# The game goes on until the player correctly guesses the number or runs out of lives.
-while player_guess != target_number and lives > 0:
-    # Display the number of guesses remaining.
-    print(f"You have {lives} attempts remaining to guess the number.")
-    # Get the user to guess the number.
-    player_guess = int(input("Make a guess: "))
-    
-    # If the player got the guess right, display a congratulatory message.
-    # Otherwise, display the appropriate feedback and subtract a life from the player.
+# If the player got the guess right, display a congratulatory message.
+# Otherwise, display the appropriate feedback and subtract a life from the player.
+def check_answer(guess, answer, lives):
     if player_guess == target_number:
         print(f"You got it! The answer was {target_number}.")
     else:
@@ -36,6 +22,29 @@ while player_guess != target_number and lives > 0:
             print("Too low.")
         print("Guess again.")
         lives -= 1
+    return lives
+
+# Get the user to choose a difficulty and give them the appropriate number of guesses.
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    lives = 0
+    if difficulty == "easy":
+        lives = 10
+    elif difficulty == "hard":
+        lives = 5
+    return lives
+    
+lives = set_difficulty()
+# The game goes on until the player correctly guesses the number or runs out of lives.
+while player_guess != target_number and lives > 0:
+    # Display the number of guesses remaining.
+    print(f"You have {lives} attempts remaining to guess the number.")
+    # Get the user to guess the number.
+    player_guess = int(input("Make a guess: "))
+    
+    # If the player got the guess right, display a congratulatory message.
+    # Otherwise, display the appropriate feedback and subtract a life from the player.
+    lives = check_answer(player_guess, target_number, lives)
      
     # If the player ran out of lives, give a game over message and display the number they were supposed to guess.
     if lives == 0:
