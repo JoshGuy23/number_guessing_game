6b64d852dd37da006830be723bc6cfac421c1985
Joshua Hecker <jhecker2001@gmail.com>
2023-10-20 08:53:45 -0500

    Created while loop and added intro text.

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -2,6 +2,8 @@ import art
 import random
 
 print(art.logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100!")
 
 player_guess = 0
 target_number = random.randint(1,100)
@@ -12,4 +14,6 @@ if difficulty == "easy":
     lives = 10
 elif difficulty == "hard":
     lives - 5
+    
+while player_guess != target_number and lives > 0:
     
